3bf2d9ab8080510890d4b5a0e6b853f3c0c05ff9
kennny <921800467@qq.com>
2017-02-04 09:31:12 +0800

    Initial Commit

---
+++ KNVideoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNVideoTest
+//
+//  Created by susie on 4/2/17.
+//  Copyright © 2017年 kennykhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KNVideoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNVideoTest
+//
+//  Created by susie on 4/2/17.
+//  Copyright © 2017年 kennykhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
